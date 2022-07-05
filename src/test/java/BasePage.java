33451b9def8c9466d303c2d4a47b67a976983c36
Andrii Myronenko <AndriiMyronenko418@gmail.com>
2022-07-05 16:02:49 +0300

    initial commit

---
+++ src/test/java/BasePage.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class BasePage {
+
+    public WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final int BASIC_TIME = 15;
+
+    //to make less code we will use $ method - to get element & $$ - to get elements (List<WebElements>)
+    public WebElement $(String locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(BASIC_TIME));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+        return driver.findElement(By.xpath(locator));
+    }
+
+    public List<WebElement> $$(String locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(BASIC_TIME));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+        return driver.findElements(By.xpath(locator));
+    }
+}
---
+++ src/test/java/TestInit.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestInit {
+
+    public WebDriver driver;
+
+    public void openUrl(String site) {
+        driver.get(site);
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void after() {
+        driver.quit();
+    }
+}
