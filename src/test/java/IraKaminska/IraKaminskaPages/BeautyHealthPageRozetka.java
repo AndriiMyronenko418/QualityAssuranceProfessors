8ca3f95d04ca52ae2ba93a50ca644283df1a2b85
Ira <ira@MacBook-Air-Ira.local>
2022-07-12 14:29:02 +0300

    all commits

---
+++ src/test/java/IraKaminska/IraKaminskaPages/BeautyHealthPageRozetka.java
@@ -0,0 +1,32 @@
+package IraKaminska.IraKaminskaPages;
+
+import BasePageAndTestInit.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BeautyHealthPageRozetka extends BasePage {
+
+    public BeautyHealthPageRozetka(WebDriver driver) {
+        super(driver);
+    }
+
+    private static final String WOMEN_PERFUMES_BUTTON = "//a[@title='Женская парфюмерия']";
+    private static final String CHANEL_PERFUMES_BUTTON = "//a[@data-id='Chanel']";
+    private static final String WOMEN_CHANEL_PERFUMES = "//h1[contains(text(), 'Женская парфюмерия Chanel')]";
+
+    public WebElement getWomenPerfumesButton() {
+        return $(WOMEN_PERFUMES_BUTTON);
+    }
+
+    public WebElement getChanelPerfumesButton() {
+        return $(CHANEL_PERFUMES_BUTTON);
+    }
+
+    public boolean checkURLChanelPerfumesButton() {
+        return driver.getCurrentUrl().contains("producer=chanel");
+    }
+
+    public WebElement checkWomenChanelPerfumes() {
+        return $(WOMEN_CHANEL_PERFUMES);
+    }
+}
