819517f04873bceb9c47f5994380eca02ef10ae8
Andrii <104580763+AndriiMyronenko418@users.noreply.github.com>
2022-07-12 15:50:13 +0300

    Update HomePage.java

--- src/test/java/ShidlovskiyMax/Pages/HomePage.java
+++ src/test/java/ShidlovskiyMax/Pages/HomePage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage {
+
     public HomePage(WebDriver driver) {
         super(driver);
-
     }
 
     public WebElement fieldkeywords() { return $("//input[@id='twotabsearchtextbox']");
