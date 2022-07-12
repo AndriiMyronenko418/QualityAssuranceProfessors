b2112d7a5a233d957730d16f208ef3f0e1fc7a29
Andrii <104580763+AndriiMyronenko418@users.noreply.github.com>
2022-07-12 15:49:38 +0300

    Update ProductSearch.java

--- src/test/java/ShidlovskiyMax/Tests/ProductSearch.java
+++ src/test/java/ShidlovskiyMax/Tests/ProductSearch.java
@@ -6,14 +6,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductSearch extends TestInit {
+
 @Test
     public void ProductSearch(){
+    
     HomePage homePage = new HomePage(driver);
+    
     openUrl("https://www.amazon.com/ref=nav_logo");
     homePage.fieldkeywords().click();
     homePage.fieldkeywords().sendKeys("Клавиатура");
     homePage.Search().click();
     Assert.assertTrue(homePage.Resultfound().isDisplayed());
+    }
 }
-}
-
