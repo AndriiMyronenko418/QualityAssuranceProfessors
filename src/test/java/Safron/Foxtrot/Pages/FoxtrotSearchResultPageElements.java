ba6e1fa678086858d73aea695d6ec60ee8ccc586
Wolmeright <105432254+Wolmeright@users.noreply.github.com>
2022-07-14 23:53:28 +0300

    Update FoxtrotSearchResultPageElements.java

--- src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageElements.java
+++ src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageElements.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class FoxtrotSearchResultPageElements extends BasePage {
+
     public FoxtrotSearchResultPageElements(WebDriver driver) {
         super(driver);
     }
