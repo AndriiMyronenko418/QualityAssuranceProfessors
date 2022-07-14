334b9abf3a79943ab5373d2dcc4041ffecf23a26
Wolmeright <105432254+Wolmeright@users.noreply.github.com>
2022-07-14 23:55:16 +0300

    Update Genshin.java

--- src/test/java/Miha/Genshin.java
+++ src/test/java/Miha/Genshin.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class Genshin extends BasePage {
+
     public Genshin(WebDriver driver) {
 
         super(driver);
