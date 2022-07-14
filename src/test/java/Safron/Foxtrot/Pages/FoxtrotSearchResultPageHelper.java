d0a7733379a6db1433033f2ea8358f54e97885b4
Wolmeright <105432254+Wolmeright@users.noreply.github.com>
2022-07-14 23:53:38 +0300

    Update FoxtrotSearchResultPageHelper.java

--- src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageHelper.java
+++ src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageHelper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class FoxtrotSearchResultPageHelper extends FoxtrotSearchResultPageElements {
+
     public FoxtrotSearchResultPageHelper(WebDriver driver) {
         super(driver);
     }
