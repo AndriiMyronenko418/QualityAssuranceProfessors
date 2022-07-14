478507c3a0e1b0ad066df073164874ac64a7957f
Wolmeright <105432254+Wolmeright@users.noreply.github.com>
2022-07-14 23:53:12 +0300

    Update FoxtrotHomePageHelper.java

--- src/test/java/Safron/Foxtrot/Pages/FoxtrotHomePageHelper.java
+++ src/test/java/Safron/Foxtrot/Pages/FoxtrotHomePageHelper.java
@@ -3,6 +3,7 @@ package Safron.Foxtrot.Pages;
 import org.openqa.selenium.WebDriver;
 
 public class FoxtrotHomePageHelper extends FoxtrotHomePageElements {
+
     public FoxtrotHomePageHelper(WebDriver driver) {
         super(driver);
     }
