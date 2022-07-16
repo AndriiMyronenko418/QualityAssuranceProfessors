a8c8dbe3a8cfe32af8e1edc754f78a24671b11a0
Ira <ira@MacBook-Air-Ira.local>
2022-07-16 19:11:16 +0300

    QAP-98_CheckBookDepartmentAmazon

--- src/test/java/IraKaminska/Amazon/PagesAmazon/DepartmentsPageAmazon.java
+++ src/test/java/IraKaminska/Amazon/PagesAmazon/DepartmentsPageAmazon.java
@@ -11,8 +11,18 @@ public class DepartmentsPageAmazon extends BasePage {
     }
 
     private static final String ELECTRONICS_DEPARTMENT = "//a[text()= 'Electronics']";
+    private static final String BOOK_DEPARTMENT = "//a[text()= 'Books']";
+    private static final String INVALID_BOOKS_PAGE = "//h2[@class='a-color-base headline truncate-1line']";
 
     public WebElement getElectronicsDepartment() {
         return $(ELECTRONICS_DEPARTMENT);
+    }
+
+    public WebElement getBookDepartment() {
+        return $(BOOK_DEPARTMENT);
+    }
+
+    public WebElement checkBookDepartment() {
+        return $(INVALID_BOOKS_PAGE);
     }
 }
