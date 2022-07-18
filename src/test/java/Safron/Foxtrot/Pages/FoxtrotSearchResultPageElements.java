336196b108f20006d2f06ebf396c9829ef57a5c7
Seraphym Tuhai <elsafronitaz@gmail.com>
2022-07-18 15:49:32 +0300

    test price

--- src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageElements.java
+++ src/test/java/Safron/Foxtrot/Pages/FoxtrotSearchResultPageElements.java
@@ -4,6 +4,8 @@ import BasePageAndTestInit.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class FoxtrotSearchResultPageElements extends BasePage {
 
     public FoxtrotSearchResultPageElements(WebDriver driver) {
@@ -11,8 +13,22 @@ public class FoxtrotSearchResultPageElements extends BasePage {
     }
 
     private static final String PRODUCT_COUNT_MESSAGE = "//label[@id='product-count-message']";
+    private static final String PRODUCTS = "//div[contains(@class,'card js-card sc-product')]";
+    private static final String WASH3676 = "//div[@data-id='3676']//a[@class='card__title']";
 
     public WebElement productCountMessage() {
         return $(PRODUCT_COUNT_MESSAGE);
+    }
+
+    public WebElement price() {
+        return $(PRODUCT_COUNT_MESSAGE);
+    }
+
+    public List<WebElement> getProducts() {
+        return $$(PRODUCTS);
+    }
+
+    public WebElement wash3676() {
+        return $(WASH3676);
     }
 }
