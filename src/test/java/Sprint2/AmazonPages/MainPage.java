dc142d83822cfc6ba5608c7a950d973ac8bce15d
Tsibatyuk <Zhtsibatyuk@gmail.com>
2022-07-19 09:12:13 +0300

    create TestUser

---
+++ src/test/java/Sprint2/AmazonPages/MainPage.java
@@ -0,0 +1,2 @@
+package Sprint2.AmazonPages;public class MainPage {
+}
---
+++ src/test/java/Sprint2/TestPage/TestUserForAmazon.java
@@ -0,0 +1,8 @@
+package Sprint2.TestPage;
+
+public class TestUserForAmazon {
+
+    protected static final String USERNAME = "ZluiBober";
+    protected static final String USER_EMAIL = "boberzhopa@gmail.com";
+    protected static final String PASSWORD = "bober1987";
+}
