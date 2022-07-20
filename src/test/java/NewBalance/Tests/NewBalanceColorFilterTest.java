ff0e051a042535a93ae4a2b6a3c1a5ca24395aac
Seraphym Tuhai <elsafronitaz@gmail.com>
2022-07-21 00:41:48 +0300

    final finished filter color test

--- src/test/java/NewBalance/Tests/NewBalanceColorFilterTest.java
+++ src/test/java/NewBalance/Tests/NewBalanceColorFilterTest.java
@@ -3,16 +3,18 @@ package NewBalance.Tests;
 import BasePageAndTestInit.TestInit;
 import NewBalance.Pages.NewBalanceHeaderViewHelper;
 import NewBalance.Pages.NewBalanceKidsPageHelper;
+import NewBalance.Pages.NewBalanceProductPageElements;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NewBalanceColorFilterTest extends TestInit {
 
     @Test
-    public void testColorFilter(){
+    public void testColorFilter() {
 
         NewBalanceHeaderViewHelper newBalanceHeaderViewHelper = new NewBalanceHeaderViewHelper(driver);
         NewBalanceKidsPageHelper newBalanceKidsPageHelper = new NewBalanceKidsPageHelper(driver);
+        NewBalanceProductPageElements newBalanceProductPageElements = new NewBalanceProductPageElements(driver);
 
         openUrl("https://newbalance.ua/");
         newBalanceHeaderViewHelper.catalogBtn("Дітям").click();
@@ -23,8 +25,9 @@ public class NewBalanceColorFilterTest extends TestInit {
         Assert.assertEquals(newBalanceKidsPageHelper.getPinkFilterColor(), newBalanceKidsPageHelper.getLabelCircleColor());
         Assert.assertTrue(newBalanceKidsPageHelper.label().getText().contains("Рожевий"));
         for (int i = 0; i < newBalanceKidsPageHelper.products().size(); i++) {
-            newBalanceKidsPageHelper.moveToProduct(i);
-            Assert.assertTrue(newBalanceKidsPageHelper.productsColor("#fa9ae7").get(i).isDisplayed()); //I stopped on this step, try do this soon
+            newBalanceKidsPageHelper.products().get(i).click();
+            Assert.assertTrue(newBalanceProductPageElements.colorLabel().getText().contains("рожевий"));
+            driver.navigate().back();
         }
     }
 }
