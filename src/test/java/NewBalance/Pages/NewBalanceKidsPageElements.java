eb262232deceab3619796d0032caae131113940f
kimkaminska <103114112+kimkaminska@users.noreply.github.com>
2022-07-21 01:01:31 +0300

    Update NewBalanceKidsPageElements.java

--- src/test/java/NewBalance/Pages/NewBalanceKidsPageElements.java
+++ src/test/java/NewBalance/Pages/NewBalanceKidsPageElements.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class NewBalanceKidsPageElements extends BasePage {
+
     public NewBalanceKidsPageElements(WebDriver driver) {
         super(driver);
     }
