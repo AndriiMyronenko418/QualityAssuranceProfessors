392ca3dcf25f906c7d53652e8c5a7f07601adfba
kimkaminska <103114112+kimkaminska@users.noreply.github.com>
2022-07-21 01:00:32 +0300

    Update NewBalanceProductPageElements.java

--- src/test/java/NewBalance/Pages/NewBalanceProductPageElements.java
+++ src/test/java/NewBalance/Pages/NewBalanceProductPageElements.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class NewBalanceProductPageElements extends BasePage {
+
     public NewBalanceProductPageElements(WebDriver driver) {
         super(driver);
     }
