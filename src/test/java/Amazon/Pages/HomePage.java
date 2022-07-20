cef18a5e0ff1ffe7ebe96fb5f2aaed1c70180ca5
Ira <ira@MacBook-Air-Ira.local>
2022-07-20 14:03:53 +0300

    QAP-146_CheckArtsAndCraftsDepartment

--- src/test/java/Amazon/Pages/HomePage.java
+++ src/test/java/Amazon/Pages/HomePage.java
@@ -1,5 +1,37 @@
 package Amazon.Pages;
 
-public class HomePage {
+import BasePageAndTestInit.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.w3c.dom.html.HTMLInputElement;
 
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    private static final String DELIVER_DONT_CHANGE_BUTTON = "//input[@class='a-button-input']";
+    private static final String ALL_MENU_BUTTON = "//span[@class='hm-icon-label']";
+    private static final String ARTS_AND_CRAFTS_DEPARTMENT = "//div[text()= 'Arts & Crafts']";
+    private static final String PAINTING_DRAWING_ART_SUPPLIES = "//a[text()= 'Painting, Drawing & Art Supplies']";
+
+    public void getDeliverDontChangeButton() {
+        if ($$(DELIVER_DONT_CHANGE_BUTTON).size() > 0) {
+            $$(DELIVER_DONT_CHANGE_BUTTON).get(0).click();
+        }
+    }
+
+    public WebElement getAllMenuButton() {
+        return $(ALL_MENU_BUTTON);
+    }
+
+    public WebElement getArtsAndCraftsDepartmentButton() {
+        return $(ARTS_AND_CRAFTS_DEPARTMENT);
+    }
+
+    public WebElement getPaintingDrawingAndArtSuppliesBtn() {
+        return $(PAINTING_DRAWING_ART_SUPPLIES);
+    }
 }
