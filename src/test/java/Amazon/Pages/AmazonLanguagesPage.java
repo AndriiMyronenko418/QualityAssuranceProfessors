4662251bdd30746ec13aa2748616108f0488dff4
Tsibatyuk <Zhtsibatyuk@gmail.com>
2022-07-21 18:11:10 +0300

    Check currency change

--- src/test/java/Amazon/Pages/AmazonLanguagesPage.java
+++ src/test/java/Amazon/Pages/AmazonLanguagesPage.java
@@ -12,12 +12,21 @@ public class AmazonLanguagesPage extends BasePage {
 
     private static final String LANGUAGE ="//span[contains(text(),'%s')]";
     private static final String TEXT_FOR_ASSERT = "//h3[contains(@id,'icp-language-heading')]";
+    private static final String CURRENCY_BTN = "//span[contains(@class,'a-button-text a-declarative')]";
 
     public WebElement language(String option){
         return $(String.format(LANGUAGE, option));
     }
 
+    public WebElement spanText(String option){
+        return $(String.format(LANGUAGE, option));
+    }
+
     public WebElement textOnPageForAssert(){
         return $(TEXT_FOR_ASSERT);
+    }
+
+    public WebElement currencyBtn(){
+        return $(CURRENCY_BTN);
     }
 }
