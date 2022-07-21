9c8c6ac91e028067dea79e657093f7bc0f5ae319
Tsibatyuk <Zhtsibatyuk@gmail.com>
2022-07-21 19:19:18 +0300

    remove one line

--- src/test/java/Amazon/Tests/CheckCurrencyChange.java
+++ src/test/java/Amazon/Tests/CheckCurrencyChange.java
@@ -17,7 +17,6 @@ public class CheckCurrencyChange extends TestInit {
                 {" - Colombian Peso", "COP"},
                 {"KRW", "KRW"},
                 {"TWD", "TWD"},
-
         };
     }
 
