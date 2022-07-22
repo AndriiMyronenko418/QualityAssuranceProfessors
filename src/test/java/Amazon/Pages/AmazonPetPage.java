08869aea54f29e8501147dcfb6634aa0118ef56d
Mike <1minato12@gmail.com>
2022-07-22 17:31:42 +0300

    code refactoring

--- src/test/java/Amazon/Pages/AmazonPetPage.java
+++ src/test/java/Amazon/Pages/AmazonPetPage.java
@@ -9,31 +9,39 @@ public class AmazonPetPage extends BasePage {
         super(driver);
     }
 
+    private static final String MoreFilters = "//div[@id='brandsRefinements']//span[@class='a-expander-prompt']";
+    private static final String ASSERTFILTER= "//li[contains(@id,'%s')]";
+    private static final String ITEMCHOSE= "//div[contains(@data-index,'%s')]//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']";
+    private static final String NAMTEITEMCHOSE= "//tr[@class='a-spacing-small po-brand']//span[@class='a-size-base']";
+    private static final String PRODUCTCHOSE= "//div[@data-asin='B0B755G6KP']";
+    private static final String PRICE= "//div[@data-asin='B0B755G6KP']//span[@class='a-price-whole']";
+    private static final String PRICENEXT= "//div[@class='a-section a-spacing-none aok-align-center']//span[@class='a-price-whole']";
+
     public WebElement getSeeMoreButtonFilters() {
-        return $("//div[@id='brandsRefinements']//span[@class='a-expander-prompt']");
+        return $(MoreFilters);
     }
 
     public WebElement filterBrandName(String option) {
-        return $(String.format("//li[contains(@id,'%s')]", option));
+        return $(String.format(ASSERTFILTER, option));
     }
 
     public WebElement getChosenButtonItems(String option) {
-        return $(String.format("//div[contains(@data-index,'%s')]//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']", option));
+        return $(String.format(ITEMCHOSE, option));
     }
 
     public WebElement brandNameChosenItems() {
-        return $("//tr[@class='a-spacing-small po-brand']//span[@class='a-size-base']");
+        return $(NAMTEITEMCHOSE);
     }
 
     public WebElement clickChosenProduct() {
-        return $("//div[@data-asin='B0B755G6KP']");
+        return $(PRODUCTCHOSE);
     }
 
     public WebElement priceProductMainPage() {
-        return $("//div[@data-asin='B0B755G6KP']//span[@class='a-price-whole']");
+        return $(PRICE);
     }
 
     public WebElement priceOnChosenProductPage() {
-        return $("//div[@class='a-section a-spacing-none aok-align-center']//span[@class='a-price-whole']");
+        return $(PRICENEXT);
     }
 }
