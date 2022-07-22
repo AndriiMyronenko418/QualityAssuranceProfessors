44b33ff25673c1d05551c554130e436ebc53661e
Andrii Myronenko <AndriiMyronenko418@gmail.com>
2022-07-22 09:58:51 +0300

    QAP-140 Amazon Stars filter test

--- src/test/java/Amazon/Pages/OutletPage.java
+++ src/test/java/Amazon/Pages/OutletPage.java
@@ -11,6 +11,11 @@ public class OutletPage extends BasePage {
     }
 
     private static final String TOP_CATEGORY_BTNS = "//a[contains(@class,'button')][contains(@title,'%s')]";
+    private static final String SHOP_BY_CATEGORY_BTNS = "//img[@alt='%s']";
+
+    public WebElement getShopByCategoryOptionBtn(String categoryName) {
+        return $(String.format(SHOP_BY_CATEGORY_BTNS, categoryName));
+    }
 
     public WebElement getTopCategoryBtn(String option) {
         return $(String.format(TOP_CATEGORY_BTNS, option));
--- src/test/java/Amazon/Pages/SearchResultsPage.java
+++ src/test/java/Amazon/Pages/SearchResultsPage.java
@@ -4,6 +4,7 @@ import BasePageAndTestInit.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchResultsPage extends BasePage {
@@ -17,6 +18,7 @@ public class SearchResultsPage extends BasePage {
     private static final String PRODUCT_BRAND = "//div[contains(@class,'product-grid-adjustment')]//span[@class]";
     private static final String NEXT_BTN = "//a[contains(@aria-label,'next')]";
     private static final String LAST_PAGE = "//span[@class='s-pagination-item s-pagination-disabled']";
+    private static final String PRODUCT_STARS = "//div[contains(@class,'main-slot')]//i[contains(@class,'star')]"; ///span
 
     public List<WebElement> getProducts() {
         return getElementsByVisibility(PRODUCT_CONTAINER);
@@ -24,6 +26,21 @@ public class SearchResultsPage extends BasePage {
 
     public List<WebElement> getProductName() {
         return $$(PRODUCT_NAME);
+    }
+
+    public List<Double> getProductStars() {
+        List<WebElement> productStars = getElementsByVisibility(PRODUCT_STARS);
+        List<String> productStarsValue = new ArrayList<>();
+        List<Double> productStarsValueDouble = new ArrayList<>();
+
+        for (int i = 0; i < productStars.size(); i++) {
+            productStarsValue.add(productStars.get(i).getAttribute("class")
+                    .replace("-", ".")
+                    .replace("a.icon a.icon.star.small a.star.small.", "")
+                    .replace(" aok.align.bottom", ""));
+            productStarsValueDouble.add(Double.parseDouble(productStarsValue.get(i)));
+        }
+        return productStarsValueDouble;
     }
 
     //method is not used because of the bug
